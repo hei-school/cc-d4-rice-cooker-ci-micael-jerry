ac0a013317d06fba2668385d36a54d872afdced7
Micael Jerry <hei.jerry.2@gmail.com>
2023-12-17 12:22:50 +0300

    fix: test

--- test/rice.cooker.service.test.js
+++ test/rice.cooker.service.test.js
@@ -1,7 +1,15 @@
 const { describe, it } = require("mocha");
+const { riceGramToLiter } = require("../src/service/rice.cooker.service");
+const { expect } = require("chai");
 
 describe('Rice cooker service TEST', () => {
   it('This test always passes', () => {
-    
+  })
+
+  describe('riceGramToLiter function TEST', () => {
+    it('Should convert the amount of rice in grams into liters', () => {
+      const res = riceGramToLiter(500);
+      expect(res).to.be.closeTo(0.8, 0.05);
+    })
   })
 })
